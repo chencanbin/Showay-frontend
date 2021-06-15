87defe0a65d9b6a8f7a6705c45197c8a3ba772ee
chencanbin <329953204@qq.com>
2021-06-15 16:35:48 +0800

    菜单栏icon支持不同状态显示

--- src/views/layout/components/Sidebar/Item.vue
+++ src/views/layout/components/Sidebar/Item.vue
@@ -1,29 +1,57 @@
+<template>
+	<div>
+		<!-- <i :class="icon" class="iconfont" v-sidebar-item="icon" /> -->
+		<svg class="iconfont" aria-hidden="true" v-sidebar-item>
+			<use :xlink:href="'#' + icon"></use>
+		</svg>
+		<span slot="title">{{ title }}</span>
+	</div>
+</template>
 <script>
+import sidebarItem from '@/directive/sidebar-item';
 export default {
-  name: "MenuItem",
-  functional: true,
-  props: {
-    icon: {
-      type: String,
-      default: "",
-    },
-    title: {
-      type: String,
-      default: "",
-    },
-  },
-  render(h, context) {
-    const { icon, title } = context.props;
-    const vnodes = [];
-
-    if (icon) {
-      vnodes.push(<i class="" class={`iconfont ${icon}`} />);
+	name: 'MenuItem',
+	// functional: true,
+	directives: {
+		sidebarItem: {
+			bind(el, binding, vNode) {
+        console.log('vNode: ', vNode);
+				const active = vNode.context.$parent.active;
+				if (active) {
+					const icon = vNode.context.icon;
+					const iconName = icon.slice(0, -4);
+					vNode.context.icon = `${iconName}_select`;
+				}
+			},
+		},
+	},
+	props: {
+		icon: {
+			type: String,
+			default: '',
+		},
+		title: {
+			type: String,
+			default: '',
+		},
+	},
+  computed: {
+    getIcon() {
+      return `#${this.icon}`;
     }
-
-    if (title) {
-      vnodes.push(<span slot="title">{title}</span>);
-    }
-    return vnodes;
   },
+	// render(h, context) {
+	//   const { icon, title } = context.props;
+	//   const vnodes = [];
+
+	//   if (icon) {
+	//     vnodes.push(<i class="" class={`iconfont ${icon}`} />);
+	//   }
+
+	//   if (title) {
+	//     vnodes.push(<span slot="title">{title}</span>);
+	//   }
+	//   return vnodes;
+	// },
 };
 </script>
